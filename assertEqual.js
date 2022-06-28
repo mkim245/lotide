3dd1b85534f1aedf11dcfde7f3e61cccd6bc3ba4
Mingu Kim <mkim245@gmail.com>
2022-06-28 15:37:22 -0400

    corrected with template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,8 @@
-// FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("🔵🔵🔵 Assertion Passed");
-  } else console.log("🔴🔴🔴 Assertion Failed");
+    console.log(`🔵🔵🔵 Assertion Passed: ${actual} === ${expected}`);
+  } else console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
 };
 
-// TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
