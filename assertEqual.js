cb64019fdc6353a68163d3260eab41be46e5ac74
Mingu Kim <mkim245@gmail.com>
2022-06-28 14:57:05 -0400

    completed function

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🔵🔵🔵 Assertion Passed");
+  } else console.log("🔴🔴🔴 Assertion Failed");
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
