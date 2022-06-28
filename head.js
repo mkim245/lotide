fce2fdf20ab4ea9e5b726c64ff86a4c5e973e917
Mingu Kim <mkim245@gmail.com>
2022-06-28 16:06:57 -0400

    head function used

---
+++ head.js
@@ -0,0 +1,9 @@
+var head = require('lodash.head');
+const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+      console.log(`🔵🔵🔵 Assertion Passed: ${actual} === ${expected}`);
+    } else console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  };
+  
+  assertEqual(head([5,6,7]), 5);
+  assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
