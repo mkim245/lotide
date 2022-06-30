2e80f282619240196e101ab90eb49210406c9178
Mingu Kim <mkim245@gmail.com>
2022-06-30 00:03:53 -0400

    countOnly added

---
+++ countOnly.js
@@ -0,0 +1,40 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🔵🔵🔵 Assertion Passed: ${actual} === ${expected}`);
+  } else console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const countOnly = function(allItems, itemsToCount) {
+  const result1 = {};
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if (result1[item]) {
+        result1[item] += 1;
+      } else {
+        result1[item] = 1;
+      }
+    }
+  }
+  return result1;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
+
+
